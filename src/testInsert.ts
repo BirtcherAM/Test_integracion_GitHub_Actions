3eadb39e86ed8e465c4bfcddd421cab8f7cc5f46
BirtcherAM <baguilonm@miumg.edu.gt>
2025-10-08 22:43:44 -0600

    Test commit git actions

--- src/testInsert.ts
+++ src/testInsert.ts
@@ -1,6 +1,6 @@
 import { crearProducto, obtenerProductos } from "./products";
 import { initDb } from "./initDb";
-
+// Script de prueba para insertar un producto y listar todos
 async function main() {
   await initDb();
   console.log("Insertando producto de prueba...");
